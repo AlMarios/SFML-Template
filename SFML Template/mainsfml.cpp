4aafe5f888555037e95f5b5b43be844f69e8770a
Александр Вертилецкий <Knight.siberia@gmail.com>
2018-03-02 01:16:25 +0700

    HOT FIX, ADD INPUT, STATE, ASSET

---
+++ SFML Template/mainsfml.cpp
@@ -0,0 +1,27 @@
+/*
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 1024
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Awesome SFML!:)");
+
+	while (window.isOpen()) {
+		// handle events
+
+		// update game logic
+
+		// draw
+		window.clear();
+
+
+		// draw SFML objects
+		window.display();
+	}
+	return 0;
+	return EXIT_SUCCESS;
+}
+*/
